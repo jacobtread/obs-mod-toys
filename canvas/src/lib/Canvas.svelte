e4c21df79270e0af8e014c9cf74701948973db90
Jacobtread <jacobtread@gmail.com>
2024-11-17 18:09:00 +1300

    feat: text object

--- canvas/src/lib/Canvas.svelte
+++ canvas/src/lib/Canvas.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
   import { canvasState, ObjectType } from "./canvas-store";
   import ImageObjectComponent from "./ImageObjectComponent.svelte";
+  import TextObjectComponent from "./TextObjectComponent.svelte";
 </script>
 
 {#each $canvasState.objects as localObject}
   {#if localObject.object.type == ObjectType.Image}
     <ImageObjectComponent {localObject} imageObject={localObject.object} />
-  {:else}{/if}
+  {:else if localObject.object.type == ObjectType.Text}
+    <TextObjectComponent {localObject} textObject={localObject.object} />
+  {/if}
 {/each}
---
+++ canvas/src/lib/TextObjectComponent.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { type LocalDefinedObject, type TextObject } from "./canvas-store";
+  import PositionedObject from "./PositionedObject.svelte";
+
+  type Props = {
+    localObject: LocalDefinedObject;
+    textObject: TextObject;
+  };
+
+  let { localObject, textObject }: Props = $props();
+  const { text } = textObject;
+</script>
+
+<PositionedObject {localObject}>
+  <p>{text}</p>
+</PositionedObject>
